0ce929caa966100799c9018d7655f5d87f2aabfd
Sameh <samkam@macbook.local>
2020-03-11 11:56:47 +0000

    update the ddi side-effects script

--- parse_ddi_descriptions.py
+++ parse_ddi_descriptions.py
@@ -1,76 +1,106 @@
 import re
 from collections import defaultdict
+from tqdm import tqdm
 
-pat_1 = re.compile('The risk or severity of .* can be increased when .* is combined with .*')
-pat_2 = re.compile('The risk or severity of .* can be decreased when .* is combined with .*')
 
-pat_3 = re.compile('.* may decrease effectiveness of .* as a diagnostic agent.')
+def sanatize_se_txt(txt):
+    return txt.strip().replace(" ", "_").lower()
 
-pat_4 = re.compile('The therapeutic efficacy of .* can be increased when used in combination with .*')
-pat_5 = re.compile('The therapeutic efficacy of .* can be decreased when used in combination with .*')
 
-pat_6 = re.compile('.* may increase the .* activities of .*')
-pat_7 = re.compile('.* may decrease the .* activities of .*')
-
-pat_8 = re.compile('The serum concentration of .* can be increased when it is combined with .*')
-pat_9 = re.compile('The serum concentration of .* can be decreased when it is combined with .*')
-
-pat_10 = re.compile('The metabolism of .* can be increased when combined with .*')
-pat_11 = re.compile('The metabolism of .* can be decreased when combined with .*')
-
-pat_12 = re.compile('The excretion of .* can be decreased when combined with .*')
-pat_13 = re.compile('The excretion of .* can be increased when combined with .*')
-
-pat_14 = re.compile('.* can cause a decrease in the absorption of .* resulting in a reduced serum concentration and potentially a decrease in efficacy.')
-
-pat_15 = re.compile('.* may decrease the excretion rate of .* which could result in a higher serum level.')
-pat_16 = re.compile('.* may increase the excretion rate of .* which could result in a lower serum level and potentially a reduction in efficacy.')
-
-pat_17 = re.compile('The protein binding of .* can be decreased when combined with .*')
-
-pat_18 = re.compile('.* can cause an increase in the absorption of .* resulting in an increased serum concentration and potentially a worsening of adverse effects.')
-
-pat_19 = re.compile('The risk of a hypersensitivity reaction to .* is increased when it is combined with .*')
-
-pat_20 = re.compile('The bioavailability of .* can be increased when combined with .*')
-pat_21 = re.compile('The bioavailability of .* can be decreased when combined with .*')
-
-pat_22 = re.compile('The absorption of .* can be decreased when combined with .*')
-
-pat_23 = re.compile('The serum concentration of the active metabolites of .* can be increased when .* is used in combination with .*')
-pat_24 = re.compile('The serum concentration of the active metabolites of .* can be reduced when .* is used in combination with .*')
-pat_25 = re.compile('The serum concentration of the active metabolites of .* can be decreased when .* is used in combination with .*')
-
-pat_26 = re.compile('The serum concentration of .* an active metabolite of .* can be increased when used in combination with .*')
-pat_27 = re.compile('The serum concentration of .* an active metabolite of .* can be decreased when used in combination with .*')
+pat_1 = re.compile('The risk or severity of (?P<se>.*) can be (?P<mode>\S+)d when .* is combined with .*')
+pat_2 = re.compile('.* may (?P<mode>\S+) (?P<se>\S+\s?\w*\s?\w*) of .* as a diagnostic agent.')
+pat_3 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of .* can be (?P<mode>\S+)d when used in combination with .*')
+pat_4 = re.compile('.* may (?P<mode>\S+) the (?P<se>.*) of .*')
+pat_5 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of .* can be (?P<mode>\S+)d when it is combined with .*')
+pat_6 = re.compile('.* can cause a decrease in the absorption of .* resulting in a (?P<mode>\S+) (?P<se>\S+\s?\w*\s?\w*) and potentially a decrease in efficacy.')
+pat_7 = re.compile('.* may decrease the excretion rate of .* which could result in a (?P<mode>\S+) (?P<se>\S+\s?\w*\s?\w*).')
+pat_8 = re.compile('.* may increase the excretion rate of .* which could result in a (?P<mode>\S+) (?P<se>\S+\s?\w*\s?\w*) and potentially a reduction in efficacy.')
+pat_9 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of .* can be (?P<mode>\S+)d when combined with .*')
+pat_10 = re.compile('.* can cause an increase in the absorption of .* resulting in an (?P<mode>\S+)d (?P<se>\S+\s?\w*\s?\w*) and potentially a worsening of adverse effects.')
+pat_11 = re.compile('The risk of a (?P<se>\S+\s?\w*\s?\w*) to .* is (?P<mode>\S+)d when it is combined with .*')
+pat_12 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of .* can be (?P<mode>\S+)d when combined with .*')
+pat_13 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of the active metabolites of .* can be (?P<mode>\S+)d when .* is used in combination with .*')
+pat_14 = re.compile('The (?P<se>\S+\s?\w*\s?\w*) of .*, an active metabolite of .* can be (?P<mode>\S+)d when used in combination with .*')
+pat_15 = re.compile('.* may (?P<mode>\S+) the central nervous system depressant (?P<se>\S+\s?\S*\s?\S*) of .*')
 
 patterns = [
     pat_1, pat_2, pat_3, pat_4, pat_5,
     pat_6, pat_7, pat_8, pat_9, pat_10,
-    pat_11, pat_12, pat_13, pat_14, pat_15,
-    pat_16, pat_17, pat_18, pat_19, pat_20,
-    pat_21, pat_22, pat_23, pat_24, pat_25,
-    pat_26, pat_27
+    pat_11, pat_12, pat_13, pat_14, pat_15
 ]
 
+mode_map = {
+    'reduced': "decrease",
+    'increase': "increase",
+    'higher': "increase",
+    'decrease': "decrease",
+    'reduce': "decrease",
+    'lower': "decrease"
+}
+
+side_effect_name_map = {
+    "central_nervous_system_depressant_(cns_depressant)_activities": 'cns_depression_activities',
+    "cns_depression": 'cns_depression_activities',
+    "cardiotoxic_activities": 'cardiotoxicity',
+    "constipating_activities": 'constipation',
+    "excretion": 'excretion_rate',
+    "hyperkalemic_activities": 'hyperkalemia',
+    "hypertensive_activities": 'hypertension',
+}
+
 pat_hits = defaultdict(int)
+se_map = dict()
+missed_lines = 0
+missed_lines_list = []
+mode_list = set()
+out_fd = open('data/preprocessed/db_ddi_se.txt', 'w')
 with open('data/preprocessed/db_interactions.txt', 'r') as input_fd:
-    for line in input_fd:
-        parts = line.strip().split('\t')
-        desc = parts[-1]
+    for line in tqdm(input_fd):
+        d1, _, d2, desc = line.strip().split('\t')
         pat_found = False
-        for index, pattern in enumerate(patterns):
-            if re.fullmatch(pattern, desc) is not None:
-                pat_hits[index] += 1
+        for pattern_index, pattern in enumerate(patterns):
+            pg = re.match(pattern, desc)
+            if pg is not None:
+                se_name_list = []
+                se_name = pg.group("se").lower()
+                mode = pg.group("mode")
+                has_word_activities = ("activities" in se_name)
+                if has_word_activities:
+                    se_name = se_name.replace(" activities", "")
+                mode_name = mode_map[mode]
+                if ", and" in se_name:
+                    se_name_list = [sanatize_se_txt(se) for se in se_name.replace("and", "").split(", ")]
+                elif "and" in se_name:
+                    se_name_list = [sanatize_se_txt(se) for se in se_name.split(" and ")]
+                else:
+                    se_name_list = [sanatize_se_txt(se_name)]
+
+                if has_word_activities:
+                    se_name_list = [txt+"_activities" for txt in se_name_list]
+
+                for side_effect in se_name_list:
+                    if side_effect in side_effect_name_map:
+                        side_effect = side_effect_name_map[side_effect]
+
+                    out_fd.write(f"{d1}\t{d2}\t{mode_name}_{side_effect}\n")
+                    if side_effect not in se_map:
+                        se_map[side_effect] = 1
+                    else:
+                        se_map[side_effect] += 1
+
+                pat_hits[pattern_index] += 1
                 pat_found = True
                 break
 
         if not pat_found:
-            print(line.strip())
+            missed_lines += 1
+            # print(line.strip())
 
+out_fd.close()
 total_hits = 0
+
 for index in range(len(patterns)):
-    print(f'{index}\t{pat_hits[index]}\t{patterns[index]}')
+    # print(f'{index}\t{pat_hits[index]}\t{patterns[index]}')
     total_hits += pat_hits[index]
 
-print(total_hits)
+print(f'total hits: {total_hits} - missed lines:{missed_lines}')
